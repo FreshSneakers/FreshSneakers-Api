4f88daa1c19e840b5e5b1d01f7183bae900725c8
rgarsan <rgarsan84@gmail.com>
2021-04-19 19:53:50 +0200

    init-express

---
+++ app.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken')
+const createError = require('http-errors');
+const logger = require('morgan');
+const express = require('express');
+
+require('./config/db.config')
+
+const app = express();
+
+/* Middlewares */
+
+app.use(express.json());
+app.use(logger('dev'));
+
+/* Routes */
+
+const routes = require('./config/routes.config')
+app.use('/api', routes)
+
+/* Handle Errors */
+
+app.use((req, res, next) => {
+    next(createError(404, 'Route not found'));
+  });
+  
+  app.use((error, req, res, next) => { //Manejamos los errores dependiendo si es error de validación o error por clave única etc.
+    if (error instanceof mongoose.Error.ValidationError) error = createError(400, error)
+    else if (error instanceof mongoose.Error.CastError) error = createError(404, 'Resource not found')
+    else if (error instanceof jwt.JsonWebTokenError) error = createError(401, error)
+    else if (error.message.includes('E11000')) error = createError(400, 'Already exists')
+    else if (!error.status) error = createError(500, error)
+  
+    if (error.status >= 500) {
+      console.error(error);
+    }
+  
+    const data = {}
+    data.message = error.message;
+    data.errors = error.errors ? 
+      Object.keys(error.errors)
+        .reduce((errors, key) => ({ ...errors, [key]: error.errors[key].message || error.errors[key] }), {}) :
+      undefined;
+  
+    res.status(error.status).json(data)
+  });
+  
+  const port = Number(process.env.PORT || 3001);
+  
+  app.listen(port, () => {
+    console.log(`Ready! Listen on port ${port}`);
+  })
