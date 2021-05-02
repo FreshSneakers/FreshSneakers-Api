7e503b2bb17773c46ee8a76a7377376bbe111aac
rgarsan <rgarsan84@gmail.com>
2021-05-03 00:42:20 +0200

    profile.css

--- data/sneakers.js
+++ data/sneakers.js
@@ -124,7 +124,146 @@ const sneakers= [
         image: 'https://stockx-360.imgix.net/Air-Jordan-New-Beginnings-Pack-1-Nike-Air-Ship/Images/Air-Jordan-New-Beginnings-Pack-1-Nike-Air-Ship/Lv2/img01.jpg?auto=compress&q=90&dpr=2&updated_at=1606316011&fit=clip&fm=webp&ixlib=react-9.0.3&w=1246',
         color: 'Red'
 
-    }
+    },
+    {
+        brand:'Adidas',
+        model:'PHARRELL X CHANEL X NMD HUMAN RACE TRAIL',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:9998,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/802756/1.jpg',
+        color:'black/running white '
+    },
+    {
+        brand:'Adidas',
+        model:'PHARRELL X NMD HUMAN RACE TRAIL FRIENDS AND FAMILY',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:1305,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/804693/1.jpg',
+        color:' black/multi-colo'
+    },
+    {
+        brand:'Adidas',
+        model:'PRADA X SUPERSTAR & BAG BUNDLE',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:3650,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/162869/1.jpg',
+        color:'core white/core white/core white'
+    },
+    {
+        brand:'Adidas',
+        model:'YEEZY BOOST 350 V2 INFANT',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:5875,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/144979/1.jpg',
+        color:'black/black/black'
+    },
+    {
+        brand:'Adidas',
+        model:'DRAGON BALL Z X ZX 500 RM',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:5000,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/805188/1.jpg',
+        color:' orange/collegiate royal/hi-res red'
+    },
+    {
+        brand:'Adidas',
+        model:'A Bathing Ape x NMD_R1',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:3500,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/201549/1.jpg',
+        color:'olive'
+    },
+    {
+        brand:'Adidas',
+        model:'NMD_R1 Primeknit Red Apple',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:2500,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/201384/1.jpg',
+        color:'bright red/white/black'
+    },
+    {
+        brand:'Adidas',
+        model:'PHARRELL X N.E.R.D. X NMD HUMAN RACE TRAIL ',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:7000,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/802550/1.jpg',
+        color:' core black/black'
+    },
+    {
+        brand:'Nike',
+        model:'DUNK LOW PREMIUM QK',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:20000,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/030382/1.jpg',
+        color:'black/white-medium gray'
+
+    },
+    {
+        brand:'Nike',
+        model:'DUNK HIGH PRO SB REVERSE SKUNK',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:18841,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/182232/1.jpg',
+        color:'cw9971 500'
+
+    },
+    {
+        brand:'Nike',
+        model:'AIR FORCE 1 LOW SUPREME I/O TZ ,MICHAEL LAU',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:14485,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/021125/1.jpg',
+        color:' white/white'
+
+    },
+    {
+        brand:'Nike',
+        model:'BEN & JERRY,S X DUNK LOW SB CHUNKY DUNKY SPECIAL ICE CREAM BOX',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:10000,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/187687/1.jpg',
+        color:'white/lagoon pulse/black/university gold'
+
+    },
+    {
+        brand:'Nike',
+        model:'DUNK HI CUSTOM SERIES',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:9990,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/030628/1.jpg',
+        color:'white/metalic smmt white-tm'
+
+    },
+    {
+        brand:'Nike',
+        model:'Nike SUPREME X NBA X AIR FORCE 1 MID 07',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:6000,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/803497/1.jpg',
+        color:'black/blac'
+
+    },
+    {
+        brand:'Nike',
+        model:'UNDEFEATED X AIR MAX 97',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:6000,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/804014/1.jpg',
+        color:'summit white/hyper cobalt-white'
+
+    },
+    {
+        brand:'Nike',
+        model:'AIR MAX PLUS TRIPLE BLACK',
+        description:'If you thought patent leather was reserved only for the Air Jordan 11, you havent met the Air Jordan 1 Retro Black Gold Patent from 2013. With a completely patent leather upper.',
+        price:2100,
+        image:'https://cdn.flightclub.com/1500/TEMPLATE/802701/1.jpg',
+        color:'black/black-black'
+
+    },
+  
+   
+  
 ]
 
 module.exports = sneakers
