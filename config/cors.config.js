a4c9c257a83c36b6d7c9bb0139ac68ada48bf042
Eduardo García <edugarciasb16@gmail.com>
2021-05-07 18:51:16 +0200

    prueba 5 cors

--- config/cors.config.js
+++ config/cors.config.js
@@ -2,6 +2,7 @@ const cors = require("cors");
 
 const corsMiddleware = cors({
     origin: 'https://fresh-sneakers-wb.herokuapp.com',
+    credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
 });
 
