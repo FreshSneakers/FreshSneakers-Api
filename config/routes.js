f25446a0a8c899d5b29dc7afe0e9b936b4ec1a65
Eduardo García <edugarciasb16@gmail.com>
2021-05-08 07:23:53 +0200

    deply heroku

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ const UsersController = require('../controllers/user.controller')
 const authMiddleware = require('../middlewares/auth.middleware')
 const ProductsController = require('../controllers/products.controller')
 const ContactController = require('../controllers/contact.controller')
+const OrdersController = require('../controllers/orders.controller')
 
 //User routes
 router.get('/users', UsersController.getUser)
@@ -11,7 +12,6 @@ router.get('/user/me', authMiddleware.isAuthenticated, UsersController.get)
 router.post('/signup', UsersController.signup)
 router.post('/login', UsersController.login)
 router.get('/activate/:token', UsersController.activate)
-
 //Products routes
 router.get('/buy', ProductsController.getBuy)
 router.post('/buy/sneaker', authMiddleware.isAuthenticated, ProductsController.buyProduct)
@@ -22,12 +22,12 @@ router.post('/sell/sneaker', authMiddleware.isAuthenticated, ProductsController.
 router.get('/sneaker-sell/:id', ProductsController.sellDetail)
 router.post('/products/webhook', express.raw({ type: 'application/json' }), ProductsController.webhook)
 //Contact US
-router.post('/contact',authMiddleware.isAuthenticated, ContactController.doContact)
-
-
+router.post('/contact', authMiddleware.isAuthenticated, ContactController.doContact)
+//Orders
+router.get('/orders-buy', authMiddleware.isAuthenticated, OrdersController.getOrdersBuy)
+router.get('/orders-sell', authMiddleware.isAuthenticated, OrdersController.getOrderSell)
 //Profile
 router.post('/profile', authMiddleware.isAuthenticated, UsersController.doEditProfile)
-
 
 
 module.exports = router
